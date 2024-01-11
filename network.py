ee30ceeee282f4e500aa843117f92d5ddf8036d9
cottelle <Cottelle@users.noreply.github.com>
2024-01-11 15:08:20 +0000

    Network.py can create a network with a number of switch pass in argument (default 7)

--- network.py
+++ network.py
@@ -9,7 +9,7 @@ def stop():
     net.net.stop()
 
 
-def main(Cli=True):
+def main(Cli=True,nb_switch=7):
     """ Start the physical network, it is a clique of 8 switchs with one host per switchs
         l2 strategy 
         Cli=True to have a Cli (!!! does not return in this case) """
@@ -17,55 +17,19 @@ def main(Cli=True):
     # Network general options
     net.setLogLevel('info')
     # net.disableArpTables()
-
-    # Network definition
-    net.addP4Switch('s1')
-    net.addP4Switch('s2')
-    net.addP4Switch('s3')
-    net.addP4Switch('s4')
-    net.addP4Switch('s5')
-    net.addP4Switch('s6')
-    net.addP4Switch('s7')
-
-    net.addHost('h1')
-    net.addHost('h2')
-    net.addHost('h3')
-    net.addHost('h4')
-    net.addHost('h5')
-    net.addHost('h6')
-    net.addHost('h7')
-
-
-    net.addLink('s1','s2')
-    net.addLink('s1','s3')
-    net.addLink('s1','s4')
-    net.addLink('s1','s5')
-    net.addLink('s1','s6')
-    net.addLink('s1','s7')
-    net.addLink('s2','s3')
-    net.addLink('s2','s4')
-    net.addLink('s2','s5')
-    net.addLink('s2','s6')
-    net.addLink('s2','s7')
-    net.addLink('s3','s4')
-    net.addLink('s3','s5')
-    net.addLink('s3','s6')
-    net.addLink('s3','s7')
-    net.addLink('s4','s5')
-    net.addLink('s4','s6')
-    net.addLink('s4','s7')
-    net.addLink('s5','s6')
-    net.addLink('s5','s7')
-    net.addLink('s6','s7')
-
-
-    net.addLink('h1','s1')
-    net.addLink('h2','s2')
-    net.addLink('h3','s3')
-    net.addLink('h4','s4')
-    net.addLink('h5','s5')
-    net.addLink('h6','s6')
-    net.addLink('h7','s7')
+    
+    switchs = []
+    for i in range(1,nb_switch+1):
+        switch_name = "s"+str(i)
+        switchs.append(switch_name)
+        net.addP4Switch(switch_name)
+        net.addHost("h"+str(i))
+        
+    
+    for i,s in enumerate(switchs):
+        net.addLink(s,"h"+str(i+1))
+        for next_sw in switchs[i+1::]:
+            net.addLink(s,next_sw)
 
 
 
@@ -83,4 +47,14 @@ def main(Cli=True):
     
     
 if __name__ == '__main__':
+    import sys
+    if len(sys.argv)>1:
+        try:
+            nb_switch = int(sys.argv[1])
+        except Exception as e:
+            print("Error passing argument ",e)
+            quit()
+        main(Cli=True,nb_switch=nb_switch)
+            
+
     main(Cli=True)
